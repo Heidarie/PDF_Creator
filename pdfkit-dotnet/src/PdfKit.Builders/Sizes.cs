0d010bc4dc46d995ec1c5475747a4f1aa4f3b438
Heidarie <dawidogly@gmail.com>
2026-02-15 23:24:48 +0100

    Sizes static added

---
+++ pdfkit-dotnet/src/PdfKit.Builders/Sizes.cs
@@ -0,0 +1,10 @@
+﻿namespace PdfKit.Builders;
+
+public static class Sizes
+{
+    public static string A4 = "A4";
+    public static string A3 = "A3";
+    public static string A5 = "A5";
+    public static string LETTER = "LETTER";
+    public static string LEGAL = "LEGAL";
+}
